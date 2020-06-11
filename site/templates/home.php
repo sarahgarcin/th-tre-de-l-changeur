6da6ff61c0209d3dfedcefdcef1461980b2ec02d
Sarah Garcin <contact@sarahgarcin.com>
2020-06-11 17:21:56 +0200

    petites modifs programmation

--- site/templates/home.php
+++ site/templates/home.php
@@ -25,35 +25,42 @@
 				<ul class='row'>
 					<?php foreach($prog->children()->listed() as $child):?>
 						<li class="col-xs-12 col-sm-6 col-md-3">
+						<?php if($image = $child->cover()->toFile()):?>
 							<div class="prog-cover-wrapper">
-								<?php $image = $child->cover()->toFile()?>
 								<?= $image->thumb([
 						      'width'   => 300,
 						      'height'  => 300,
 						      'quality' => 80
 						    ])->html();?>
 							</div>
+						<?php endif;?>
 							<div class="prog-infos-wrapper">
 								<h4><?= $child->genre()->html()?></h4>
 								<h2><?= $child->title()->html()?></h2>
-								<h3><?= $child->dates()->html()?></h3>
+								<h3>
+									<?= $child->dates()->html()?><br>
+									<span class="hours"><?= $child->hours()->kt()?></span>
+								</h3>
+								<div class="distribution"><?= $child->distribution()->kt()?></div>
+								
 								<?php if($child->pdfs()->isNotEmpty()):?>
 									<ul class="pdfs-wrapper">
 										<?php foreach($child->pdfs()->toStructure() as $pdf):?>
-											<li>
-												<?php $file = $pdf->pdf()->toFile();?>
-												<a href="<?= $file->url() ?>" title="<?= $pdf->title()?>" target="_blank">
-													<?= $pdf->title() ?>
-												</a>
-											</li>
+											<?php if($file = $pdf->pdf()->toFile()):?>
+												<li>
+													<a href="<?= $file->url() ?>" title="<?= $pdf->title()?>" target="_blank">
+														<?= $pdf->title() ?>
+													</a>
+												</li>
+											<?php endif;?>
 										<?php endforeach;?>
 									</ul>
+								<?php endif;?>
 								<div class="lien-billeterie">
 									<a href="<?= $child->billeterie()?>" title="<?= $child->title()?>" target="_blank">
 										Réservez votre billet
 									</a>
 								</div>
-								<?php endif;?>
 							</div>
 						</li>
 					<?php endforeach?>
